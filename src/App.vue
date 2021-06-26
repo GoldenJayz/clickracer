bce9a930f2811e7c1153e848fec2e0af6cc2e86c
Jaden D <45317852+GoldenJayz@users.noreply.github.com>
2021-06-25 20:39:35 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <fart />
+  </div>
+</template>
+
+<script>
+import fart from './components/fart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    fart
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/fart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <button @click="ging++">click here</button>
+      {{ ging }}
+      <p>
+        {{ ouser }}
+      </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data: function() {
+        return {
+            ging: 0
+        }
+    }
+}
+//make a program that makes a counter for each time you click a button
+</script>
+
+<style scoped>
+
+</style>
